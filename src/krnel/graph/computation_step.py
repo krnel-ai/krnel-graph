e54c296a9c3dd9fb2abae127fbdd9d1d64f9b153
Kimberly Wilber <kimmy@kjwilber.org>
2025-07-29 15:03:40 -0400

    All graph computations now contain a `type` field so they can be deserialized.

--- src/krnel/graph/computation_step.py
+++ src/krnel/graph/computation_step.py
@@ -3,13 +3,19 @@ from functools import cached_property
 import hashlib
 import json
 from typing import Any
-from pydantic import BaseModel, ConfigDict, SerializationInfo, SerializerFunctionWrapHandler, field_serializer
+from pydantic import BaseModel, ConfigDict, SerializationInfo, SerializerFunctionWrapHandler, ValidatorFunctionWrapHandler, field_serializer, model_serializer, model_validator
 
 class ComputationSpec(BaseModel):
     """
     Base class for all computation specifications.
     Each ComputationSpec is content-addressable, meaning it can be uniquely identified by its content.
+
+    Class names of all subclasses of ComputationSpec must be globally unique.
     """
+
+    model_config = ConfigDict(
+        frozen = True,  # Makes instances immutable
+    )
 
     @field_serializer('*', mode='wrap')
     def serialize_for_hash(
@@ -43,6 +49,43 @@ class ComputationSpec(BaseModel):
         short_content_digest = base64.b64encode(content_digest, altchars=b'-_').decode('utf-8')
         return "step-" + short_content_digest.rstrip('=')
 
-    model_config = ConfigDict(
-        frozen = True,  # Makes instances immutable
-    )
+    # The below adds a "type" field to the serialized output that
+    # can be used to identify the specific subclass of ComputationSpec
+    # during both serialization and deserialization.
+    # https://github.com/pydantic/pydantic/issues/7366#issuecomment-1742596823
+
+    @model_serializer(mode='wrap')
+    def inject_type_on_serialization(
+        self, handler: ValidatorFunctionWrapHandler
+    ) -> dict[str, Any]:
+        result: dict[str, Any] = handler(self)
+        print('...', result)
+        if 'type' in self.__class__.model_fields:
+            raise ValueError('Cannot use field "type". It is reserved.')
+        result['type'] = f'{self.__class__.__name__}'
+        return result
+
+    @model_validator(mode='wrap')  # noqa  # the decorator position is correct
+    @classmethod
+    def retrieve_type_on_deserialization(
+        cls, value: Any, handler: ValidatorFunctionWrapHandler
+    ) -> "ComputationSpec":
+        def find_subclass(cls: type, name: str) -> type:
+            if cls.__name__ == name:
+                return cls
+            for subclass in cls.__subclasses__():
+                if found := find_subclass(subclass, name):
+                    return found
+        if isinstance(value, dict):
+            # WARNING: we do not want to modify `value` which will come from the outer scope
+            # WARNING2: `sub_cls(**modified_value)` will trigger a recursion, and thus we need to remove `type`
+            modified_value = value.copy()
+            sub_cls_name = modified_value.pop('type', None)
+            if sub_cls_name is not None:
+                sub_cls = find_subclass(cls=ComputationSpec, name=sub_cls_name)
+                if sub_cls is not None:
+                    return sub_cls(**modified_value)
+                raise ValueError(f'Subclass with name {sub_cls_name} not found in {cls.__name__} hierarchy.')
+            else:
+                return handler(value)
+        return handler(value)

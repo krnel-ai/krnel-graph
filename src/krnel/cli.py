3853b4284d08a24c4e12b4fe7edf79d340ae647b
Kimberly Wilber <kimmy@kjwilber.org>
2025-08-12 17:54:10 -0400

    nicer output for krnel 'status' command

--- src/krnel/cli.py
+++ src/krnel/cli.py
@@ -1,7 +1,16 @@
-from krnel import graph
-import warnings
-from rich import print
+# Copyright (c) 2025 Krnel
+# Points of Contact:
+#   - kimmy@krnel.ai
 
+from datetime import datetime, timezone
+import warnings
+from rich.tree import Tree
+from rich import print
+import humanize
+
+from krnel import graph
+from krnel.graph.graph_transformations import map_fields
+from krnel.graph.op_spec import OpSpec
 from krnel.runners import LocalArrowRunner
 
 try:
@@ -16,31 +25,52 @@ except ImportError:
 app = App( name="krnel")
 
 @app.command
-def status(store_uri: str, op_uuid: list[str]):
+def status(store_uri: str, *op_uuid: str):
     runner = LocalArrowRunner(store_uri=store_uri)
     ops = []
     for uuid in op_uuid:
         op = runner.uuid_to_op(uuid)
         if op is None:
-            print(f"Operation with UUID {op_uuid} not found.")
+            print(f"Operation with UUID {uuid} not found.")
             return
         ops.append(op)
 
+    def format_time(status):
+        match [status.time_started, status.time_completed]:
+            case [None, None]:
+                return "not started"
+            case [time_started, None]:
+                return f"for {humanize.naturaldelta(datetime.now(timezone.utc) - time_started)}"
+            case [time_started, time_completed]:
+                return f"{humanize.naturaldelta(datetime.now(timezone.utc) - time_completed)} ago, took {humanize.naturaldelta(time_completed - time_started)}"
+
     seen = set()
-    def _visit(op):
-        if op in seen:
-            return
+    def show_one(op, tree, name=""):
+        if op.uuid in seen:
+            return tree
+        seen.add(op.uuid)
         status = runner.get_status(op)
-        if status != 'completed':
-            for dep in op.get_dependencies():
-                _visit(dep)
-        if status is None:
-            print(f"No status found for operation {op.uuid}.")
-        print(status)
-        print()
-        seen.add(op)
+        time = format_time(status)
+        match status.state:
+            case 'completed':
+                branch = tree.add(f"{name}: [green]{status.state}[/green] {time}")
+            case 'ephemeral':
+                branch = tree.add(f"{name}: [green]{status.state}[/green] {time}")
+            case 'running':
+                branch = tree.add(f"{name}: [blue]{status.state}[/blue] {time}")
+            case _:
+                branch = tree.add(f"{name}: [yellow]{status.state}[/yellow] {time}")
+        for fieldname in op.__class__.model_fields:
+            child = getattr(op, fieldname)
+            map_fields(
+                child,
+                OpSpec,
+                lambda x: show_one(x, branch, name=fieldname),
+            )
+        return tree
+
     for op in ops:
-        _visit(op)
+        print(show_one(op, Tree(op.uuid)))
 
 @app.command
 def materialize(store_uri: str, *op_uuid: str):

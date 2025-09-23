530533ce4c1e6cc2168fb3aaab210033f6769339
Kimberly Wilber <kimmy@kjwilber.org>
2025-09-23 16:01:06 -0400

    BUGFIX (UNVERIFIED): "ghost results" (without any status) should be completed, not new
    implementations could write results for other ops,
    which wouldn't put_result(). at least now we can detect whether they
    have results

--- src/krnel/graph/runners/local_runner/local_arrow_runner.py
+++ src/krnel/graph/runners/local_runner/local_arrow_runner.py
@@ -236,7 +236,7 @@ class LocalArrowRunner(BaseRunner):
             return status  # noqa: RET504
         else:
             log.debug("status not found, creating new")
-            new_status = OpStatus(op=op, state="new")
+            new_status = OpStatus(op=op, state="new" if not self.has_result(op) else "completed")
             self.put_status(new_status)
             return new_status
 

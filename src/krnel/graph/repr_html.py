a78317e634d8cc9813fa2da2fc7ea4441a27268f
Kimberly Wilber <kimmy@kjwilber.org>
2025-09-23 16:01:06 -0400

    fix: no longer require VSCode mermaid markdown extension

--- src/krnel/graph/repr_html.py
+++ src/krnel/graph/repr_html.py
@@ -2,27 +2,65 @@
 # Points of Contact:
 #   - kimmy@krnel.ai
 
+import uuid
+import json
+
 _TEMPLATE = """
-```mermaid
 ---
 config:
   nodeSpacing: 1
-  padding: 1
+  nodePadding: 1
+  layout: elk
 ---
 flowchart RL
 {nodes}
 {edges}
-```"""
-
+"""
 
 class FlowchartReprMixin:
-    def _repr_markdown_(self):
+    # def _repr_html_(self):
+    #     nodes = []
+    #     edges = []
+    #     for node in self.get_dependencies(recursive=True) + [self]:
+    #         nodes.append(node._repr_flowchart_node_())
+    #         edges.extend(list(node._repr_flowchart_edges_()))
+    #     return _TEMPLATE.format(nodes="\n".join(nodes), edges="\n".join(edges))
+
+    def _repr_mimebundle_(self, include=None, exclude=None):
         nodes = []
         edges = []
         for node in self.get_dependencies(recursive=True) + [self]:
             nodes.append(node._repr_flowchart_node_())
             edges.extend(list(node._repr_flowchart_edges_()))
-        return _TEMPLATE.format(nodes="\n".join(nodes), edges="\n".join(edges))
+
+        elem_id = f"mermaid-{uuid.uuid4().hex}"
+
+        html_bundle = f"""
+        <div id="{elem_id}">
+            <pre>{repr(self)}</pre>
+        </div>
+        <script>
+        (async () => {{
+          console.log("HELLO", document, window);
+          const el = document.getElementById("{elem_id}");
+          el.innerHTML = {json.dumps(_TEMPLATE.format(nodes="\n".join(nodes), edges="\n".join(edges)))};
+
+          const mod = await import("https://cdn.jsdelivr.net/npm/mermaid@11/dist/mermaid.esm.min.mjs");
+          let mermaid = mod.default || mod;
+
+          try {{
+            mermaid.initialize({{ startOnLoad: false }});
+            await mermaid.run({{ nodes: [el] }});
+          }} catch (err) {{
+            console.error("mermaid render error:", err);
+          }}
+        }})();
+        </script>
+        """
+        return {
+            "text/html": html_bundle,
+            #"application/javascript": js,
+        }
 
 
 class FlowchartBigNode:
